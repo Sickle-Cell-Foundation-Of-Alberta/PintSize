21cc4940d369188b51d44db157354e3d08c04685
Head8che <hagi@ualberta.ca>
2021-07-19 22:55:22 -0600

    revert to original code

--- lib/Screens/Home/homeTabs/homepageAwareness.dart
+++ lib/Screens/Home/homeTabs/homepageAwareness.dart
@@ -1,123 +1,76 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:pintsize/Screens/Home/subPages/awareness.dart';
 
-class AwarenessPage extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<AwarenessPage> {
-  //Creating an instance of firebase
-  final databaseReference = FirebaseDatabase.instance.reference();
-
+class AwarenessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: formWidget(),
-    );
-  }
+    var size = MediaQuery.of(context).size;
 
-//Creating a referance to the database child
-  var _firebaseRef =
-      FirebaseDatabase().reference().child('Awareness').orderByChild("id");
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
 
-  Widget formWidget() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0, left: 6.0, right: 6.0),
-      child: Center(
+    return Scaffold(
+      backgroundColor: Color(0xFFFCFAF8),
+      body: new Container(
         child: StreamBuilder(
-          stream: _firebaseRef.onValue,
-/*On value change the stream is triggered for all 
-crud operations. 
-If you only want to trigger when a child is added 
-use ***stream: _firebaseRef.onChildAdded,*** instead of onValue*/
-          builder: (context, snap) {
-            if (snap.hasData &&
-                !snap.hasError &&
-                snap.data.snapshot.value != null) {
-              Map data = snap.data.snapshot.value;
-              List item = [];
-
-              data.forEach((index, data) {
-                item.add(
-                    {"key": index, ...data}); //The items are added to the list
-
-                //if you only want to add a specific type of data to be displayed you can use this approach
-                /*if (data["Type"] == "Pollution") {
-                  item.add({"key": index, ...data});
-                } */
-              });
-              if (item.length > 0) {
-                return ListView.builder(
-                  itemCount: item.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                        padding: EdgeInsets.only(
-                            top: 10.0, bottom: 15.0, left: 45.0, right: 45.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => HomeAwarenessSubPage(
-                                    documentTitle: item[index][
-                                        "title"], //The title is taken from the list index
-                                    documentDescription: item[
-                                            index] //The description is taken from the list index
-                                        ["description"])));
-                          },
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.grey.withOpacity(0.2),
-                                        spreadRadius: 3.0,
-                                        blurRadius: 5.0)
-                                  ],
-                                  color: Colors.white),
-                              child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SizedBox(height: 1.0),
-                                    Align(
-                                      alignment: Alignment.center,
-                                      child: Text(item[index]["title"],
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Color(0xFFCC8053),
-                                              fontFamily: 'Varela',
-                                              fontSize: 14.0)),
-                                    ),
-                                  ])),
-                        ));
-                  },
-                );
-              } else {
-                //if item list has no data
-                return Container(
-                  child: ListView(children: <Widget>[
-                    Text(
-                      "Nothing To Display",
-                      style: TextStyle(height: 5, fontSize: 30),
-                      textAlign: TextAlign.center,
-                    ),
-                  ]),
-                );
-              }
-            } else //if database reading has no data
-              return Container(
-                child: ListView(children: <Widget>[
-                  Text(
-                    "Nothing To Display",
-                    style: TextStyle(height: 5, fontSize: 30),
-                    textAlign: TextAlign.center,
-                  ),
-                ]),
-              );
+          stream:
+              FirebaseFirestore.instance.collection('Awareness').snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (!snapshot.hasData) return const SizedBox.shrink();
+            return GridView.builder(
+                itemCount: snapshot.data!.docs.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: MediaQuery.of(context).size.width /
+                        (MediaQuery.of(context).size.height / 5)),
+                itemBuilder: (BuildContext context, int index) {
+                  final databaseQuery = snapshot.data!.docs[index].data();
+                  return _buildCard(databaseQuery['title'],
+                      databaseQuery['description'], context);
+                });
           },
         ),
       ),
     );
   }
+}
+
+Widget _buildCard(String title, String description, context) {
+  return Padding(
+      padding:
+          EdgeInsets.only(top: 10.0, bottom: 15.0, left: 45.0, right: 45.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => HomeAwarenessSubPage(
+                  documentTitle: title, documentDescription: description)));
+        },
+        child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 3.0,
+                      blurRadius: 5.0)
+                ],
+                color: Colors.white),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(height: 1.0),
+              Align(
+                alignment: Alignment.center,
+                child: Text(title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFCC8053),
+                        fontFamily: 'Varela',
+                        fontSize: 14.0)),
+              ),
+            ])),
+      ));
 }
