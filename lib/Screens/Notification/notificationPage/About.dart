3a9941af641c84b3a72053192214b7b2b7b917e2
padjaye <84160025+padjaye@users.noreply.github.com>
2021-08-06 14:58:09 -0600

    about page

--- lib/Screens/Notification/notificationPage/About.dart
+++ lib/Screens/Notification/notificationPage/About.dart
@@ -2,82 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:pintsize/Screens/Notification/subPage/notification.dart';
 
-class AboutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-
-    /*24 is for notification bar on Android*/
-    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
-    final double itemWidth = size.width / 2;
-
-    return Scaffold(
-      backgroundColor: Color(0xFFFCFAF8),
-      body: new Container(
-        child: StreamBuilder(
-          stream:
-              FirebaseFirestore.instance.collection('Awareness').snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (!snapshot.hasData) return const SizedBox.shrink();
-            return GridView.builder(
-                itemCount: snapshot.data!.docs.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    childAspectRatio: MediaQuery.of(context).size.width /
-                        (MediaQuery.of(context).size.height / 5)),
-                itemBuilder: (BuildContext context, int index) {
-                  final databaseQuery = snapshot.data!.docs[index].data();
-                  return _buildCard(databaseQuery['Title'],
-                      databaseQuery['Description'], context);
-                });
-          },
-        ),
-      ),
-    );
-  }
-}
-
-Widget _buildCard(String title, String description, context) {
-  return Padding(
-      padding:
-          EdgeInsets.only(top: 10.0, bottom: 15.0, left: 45.0, right: 45.0),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => NotificationSubPage(
-                  documentTitle: title, documentDescription: description)));
-        },
-        child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 3.0,
-                      blurRadius: 5.0)
-                ],
-                color: Colors.white),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SizedBox(height: 1.0),
-              Align(
-                alignment: Alignment.center,
-                child: Text(title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFCC8053),
-                        fontFamily: 'Varela',
-                        fontSize: 14.0)),
-              ),
-              Center(
-                child: Text(description,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Varela',
-                        fontSize: 15.0)),
-              ),
-            ])),
-      ));
-}
+void main() { runApp(AboutPage()); }  
+  
+class AboutPage extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return MaterialApp(  
+        theme: ThemeData(  
+          primarySwatch: Colors.red,  
+        ),  
+        home: MyTextPage()  
+    );  
+  }  
+}  
+class MyTextPage extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return Scaffold(  
+         
+      body: Center(  
+          child: Text("The Sickle Cell Foundation of Alberta (SCFA) is a volunteer-based organization dedicated to creating awareness about SCD and providing support services to patients living in Alberta. It was founded in 2001 by Dr. Ekua Yorke and Dr. Jojo Yorke to help patients deal with their condition.The SCFA teaches patients coping skills for pain management, advocates for funding of SCD research and provides its members with access to the most current scientific research and information. It is led by a board of directors, consisting primarily of sickle cell patients, health professionals, patients’ parents and caregivers.",  
+     textAlign: TextAlign.left,
+     style: TextStyle(color:Colors.black87, fontSize:20),
+      )),  
+    );  
+  }  
+}  
